f55ad3f518a2e4bc4402e795c63bf834f7e8aa27
Faith Best <97316553+ibringfaith@users.noreply.github.com>
2024-12-18 00:40:41 +0000

    Add the code from the associated instruction number and generate plots

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -7,30 +7,27 @@ import numpy as np
 df = pd.read_csv('medical_examination.csv', sep=',')
 
 # 2
-bmi = df['weight']/(df['height']**2)
-df['overweight'] = 0 if bmi <=25 else 1
+bmi = df['weight']/((df['height']/100)**2)
+df['overweight'] = (bmi > 25).astype(int)
 
 # 3
 # Normalize data by making 0 always good and 1 always bad. 
-# If the value of cholesterol or gluc is 1, set the value to 0. If the value is more than 1, set the value to 1.
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=["cardio"], value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"])
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index=False).size()
+    df_cat.rename(columns={"size": "total"}, inplace=True)
     
-
     # 7
 
-
-
     # 8
-    fig = None
-
+    fig = sns.catplot(x="variable", y="total", hue="value", col="cardio", data=df_cat, kind="bar").figure
 
     # 9
     fig.savefig('catplot.png')
@@ -40,22 +37,22 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    # filter out the patient segments that represent incorrect data
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & 
+                 (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & 
+                 (df['weight'] <= df['weight'].quantile(0.975))]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(10, 8))
 
     # 15
-
-
+    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", cmap="coolwarm", cbar=True, ax=ax)
 
     # 16
     fig.savefig('heatmap.png')
